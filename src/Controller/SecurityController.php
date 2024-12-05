07c4b2d92d7b6e956bb2b0357c6bd1eadb79b14e
neb <nebrass@tekup.de>
2024-12-05 11:01:24 +0100

    login test

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,19 +13,15 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // If the user is already logged in, redirect to a specific route
-        if ($this->getUser()) {
-            return $this->redirectToRoute('target_path'); // Replace 'target_path' with your desired route
-        }
+        /*if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }*/
 
         // Get the login error, if any
         $error = $authenticationUtils->getLastAuthenticationError();
         // Retrieve the last username entered
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        // Redirect to /register if no username and no error (user might not have an account)
-        if (!$lastUsername && !$error) {
-            return $this->redirectToRoute('app_register');
-        }
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
